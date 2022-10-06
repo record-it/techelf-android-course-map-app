89212630359625ea40b2da16833138e9b0041fa8
cs <siwon.cezary@gmail.com>
2022-10-06 19:20:04 +0200

    init

--- app/src/main/java/pl/recordit/examples/techlif/maps/MapsActivity.java
+++ app/src/main/java/pl/recordit/examples/techlif/maps/MapsActivity.java
@@ -58,50 +58,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
         );
         inWarsaw.setTag("Dowolne dane");
-        String data = (String) inWarsaw.getTag();
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(warsaw);
-        mMap.animateCamera(CameraUpdateFactory.zoomIn());
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(3), 2000, null);
-        mMap.moveCamera(cameraUpdate);
-        mMap.setOnCameraMoveStartedListener((e) -> {
-            Toast.makeText(this.getBaseContext(), "Camera start move event", Toast.LENGTH_SHORT).show();
-        });
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(warsaw)
-                .zoom(7)
-                .bearing(90)
-                .tilt(0)
-                .build();
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-        LatLngBounds adelaideBounds = new LatLngBounds(
-                new LatLng(49, 16), // SW bounds
-                new LatLng(54, 23)  // NE bounds
-        );
-        mMap.setLatLngBoundsForCameraTarget(adelaideBounds);
-        UiSettings uiSettings = mMap.getUiSettings();
-        uiSettings.setZoomControlsEnabled(true);
-        uiSettings.setCompassEnabled(true);
-        uiSettings.setIndoorLevelPickerEnabled(true);
-        uiSettings.setMapToolbarEnabled(true);
-        uiSettings.setMyLocationButtonEnabled(true);
-
-        mMap.setOnMapClickListener((e) -> {
-            Toast.makeText(this.getBaseContext(), "Click on map", Toast.LENGTH_SHORT).show();
-        });
-        mMap.setOnMarkerClickListener(e -> {
-            Toast.makeText(this.getBaseContext(), "Click on marker", Toast.LENGTH_SHORT).show();
-            return true;
-        });
-        CircleOptions circleOptions = new CircleOptions()
-                .center(new LatLng(37.4, -122.1))
-                .radius(1000);      // metry
-        Circle circle = mMap.addCircle(circleOptions);
-        PolygonOptions polygonOptions = new PolygonOptions()
-                .add(new LatLng(37.35, -122.0),
-                        new LatLng(37.45, -122.0),
-                        new LatLng(37.45, -122.2),
-                        new LatLng(37.35, -122.2),
-                        new LatLng(37.35, -122.0));
-        Polygon polygon = mMap.addPolygon(polygonOptions);
     }
 }
